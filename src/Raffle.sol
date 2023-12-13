bcc66b04d5b141959f62efe5c2a74e67c3361d96
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-13 16:42:26 +0100

    Basic Raffle empty functions created

---
+++ src/Raffle.sol
@@ -0,0 +1,55 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+* @title Raffle Contract 
+* @author Jordi Martinez 
+* @notice Creating a verified random raffle using Chainlink Oracles
+* @dev Implements Chainlink VRFv2
+ */
+
+contract Raffle{
+
+    uint private immutable i_entranceFee;
+
+    constructor(uint entranceFee){
+        i_entranceFee = entranceFee;
+
+    }
+    function enterRaffle() public{
+
+    }
+
+    function pickWinner() public {
+
+    }
+
+    /** Getter functions */
+
+    function getEntranceFee() public view returns (uint){
+        return i_entranceFee;
+    }
+
+}
