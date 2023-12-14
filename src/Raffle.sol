9b6f2b10ad0bc275ec440ab93154e20b266ea5b3
JordiMartinezDev <jordiupc87@gmail.com>
2023-12-14 06:49:32 +0100

    revert enterRaffle throwing error, more gas efficient

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,16 +30,24 @@ pragma solidity ^0.8.18;
 * @dev Implements Chainlink VRFv2
  */
 
+
 contract Raffle{
 
+    error Raffle__EntranceFeeNotEnough();
+
     uint private immutable i_entranceFee;
+
+    
 
     constructor(uint entranceFee){
         i_entranceFee = entranceFee;
 
     }
     function enterRaffle() external payable{
-        require(msg.value >= i_entranceFee, "Not enopugh money");
+        //require(msg.value >= i_entranceFee, "Not enopugh money");
+        if(msg.value < i_entranceFee){
+            revert Raffle__EntranceFeeNotEnough();
+        }
     }
 
     function pickWinner() public {
